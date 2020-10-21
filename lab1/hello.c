8ab0ce792b59a3dae086ea7ad5fe248c76e70c37
Muhammmad Haniff Bin Ruslan <2018238426@isiswa.uitm.edu.my>
2020-10-21 03:48:52 +0000

    My first commit

---
+++ lab1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void ) {
+/* This is my first programm in C */
+printf("hello world!");
+printf("I love C");
+return (0);
+}
---
+++ lab1/userinput.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+/* This is my second program in C */
+int age;
+printf("hi, how old are you? >");
+scanf("%d" , &age);
+printf("you are %d years old", age);
+return (0);
+}
